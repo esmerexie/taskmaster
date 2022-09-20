b2ad4420c875f93fddd6b52c3150d8afe39cc11f
esmerexie <esmeraldarexie@gmail.com>
2022-09-19 23:58:25 -0700

    completed lab

---
+++ app/src/main/java/com/example/taskmaster/AddTaskActivity.java
@@ -0,0 +1,30 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+
+        Button addTaskSubmitButton = AddTaskActivity.this.findViewById(R.id.addTaskSubmitButton);
+        addTaskSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context submitted = getApplicationContext();
+                CharSequence text = "Task Submitted!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(submitted, text, duration);
+                toast.show();
+            }
+        });
+    }
+}
