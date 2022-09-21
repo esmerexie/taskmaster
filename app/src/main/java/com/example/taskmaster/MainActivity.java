2898662ba66353e0bf04413c11e0466c7aa71536
esmerexie <esmeraldarexie@gmail.com>
2022-09-21 00:26:29 -0700

    almost done with lab

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -3,30 +3,79 @@ package com.example.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String PRODUCT_NAME_EXTRA_TAG = "productName";
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button addTaskBttn = MainActivity.this.findViewById(R.id.mainActivityAddTaskButton);
+       sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-        addTaskBttn.setOnClickListener(view -> {
+       createAddTaskButton();
+       createAllTaskButton();
+       createTaskOneButton();
+       createSettingsButton();
 
-            Intent goToAddTaskPage = new Intent(MainActivity.this, AddTaskActivity.class);
-            startActivity(goToAddTaskPage);
-        });
-
-        Button allTaskBttn = MainActivity.this.findViewById(R.id.MainActivityAllTaskButton);
-
-        allTaskBttn.setOnClickListener(view -> {
-
-            Intent goToAllTasksPage = new Intent(MainActivity.this, AllTasksActivity.class);
-            startActivity(goToAllTasksPage);
-        });
     }
-}
+
+//    @Override
+//    protected void onResume(){
+//        super.onResume();
+//        String userName = sharedPreferences.getString(SettingsPageActivity.USER_NAME_TAG, "No username");
+//        TextView userNameEdited = findViewById(R.id.mainActivityUsernameTextView);
+//        userNameEdited.setText(userName + "'s Tasks");
+//    }
+
+
+    private void createAddTaskButton() {
+            Button addTaskBttn = MainActivity.this.findViewById(R.id.mainActivityAddTaskButton);
+
+            addTaskBttn.setOnClickListener(view -> {
+
+                Intent goToAddTaskPage = new Intent(MainActivity.this, AddTaskActivity.class);
+                startActivity(goToAddTaskPage);
+            });
+        }
+
+    private void createAllTaskButton(){
+                Button allTaskBttn = MainActivity.this.findViewById(R.id.MainActivityAllTaskButton);
+
+                allTaskBttn.setOnClickListener(view -> {
+
+                    Intent goToAllTasksPage = new Intent(MainActivity.this, AllTasksActivity.class);
+                    startActivity(goToAllTasksPage);
+                });
+            }
+
+            private void createTaskOneButton(){
+                Button taskOneButton = MainActivity.this.findViewById(R.id.mainActivityTaskOneButton);
+
+                taskOneButton.setOnClickListener(view -> {
+                    Intent goToTaskPage = new Intent(MainActivity.this, TaskDetailPageActivity.class);
+                    startActivity(goToTaskPage);
+                });
+            }
+
+            private void createSettingsButton(){
+                Button settingsButton = MainActivity.this.findViewById(R.id.mainActivitySettingsButton);
+
+                settingsButton.setOnClickListener(view -> {
+                    Intent goToSettingsPage = new Intent(MainActivity.this, SettingsPageTwoActivity.class);
+                    startActivity(goToSettingsPage);
+                });
+            }
+
+
+
+        }
+
